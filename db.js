c26232483aeed11bf501e92331c14ac3983e6ce8
totato4 <v4.97@list.ru>
2024-02-08 07:43:01 +0300

    vercel

--- db.js
+++ db.js
@@ -1,10 +1,14 @@
 const Pool = require("pg").Pool;
+// const pool = new Pool({
+//   user: "postgres",
+//   password: "nemongo",
+//   host: "localhost",
+//   port: 5432,
+//   database: "sasa",
+// });
+
 const pool = new Pool({
-  user: "postgres",
-  password: "nemongo",
-  host: "localhost",
-  port: 5432,
-  database: "sasa",
+  connectionString: process.env.POSTGRES_URL,
 });
 
 // const Pool = require("pg").Pool;
