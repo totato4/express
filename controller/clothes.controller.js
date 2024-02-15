ff7384646953a7a9ef71376d179fae3f5992d124
totato4 <v4.97@list.ru>
2024-02-15 09:59:06 +0300

    work

--- controller/clothes.controller.js
+++ controller/clothes.controller.js
@@ -3,9 +3,14 @@ const db = require("../db");
 class ClothesController {
   async getClothesByHuman_c(req, res) {
     const human_c = req.params.human_c;
-    const clothes = await db.query(`select * from clothes where human_c = $1`, [
+    const clothes = await db.query(`SELECT * FROM clothes where human_c = $1`, [
       human_c,
     ]);
+    res.json(human_c);
+  }
+
+  async getAllClothes(req, res) {
+    const clothes = await db.query(`SELECT * FROM clothes`);
     res.json(clothes.rows);
   }
 }
