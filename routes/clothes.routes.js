9717994f33410844e844a90d8ccaa19b6e7433aa
totato4 <v4.97@list.ru>
2024-02-15 10:03:50 +0300

    work

--- routes/clothes.routes.js
+++ routes/clothes.routes.js
@@ -3,5 +3,6 @@ const router = new Router();
 const clothesController = require("../controller/clothes.controller");
 
 router.get("/clothes", clothesController.getClothesByHuman_c);
+router.get("/clothes", clothesController.getAllClothes);
 
 module.exports = router;
